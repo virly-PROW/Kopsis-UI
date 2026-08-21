9d3154b09a8a77466b0660fd0a356f677ef04770
virly qoirul <virlyyannisa@gmail.com>
2026-08-21 07:15:35 +0700

    LKM-3 Kartu Barang

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  int stok = 40;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: Container(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('Buku Tulis'),
-              const SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text('Anggota: Rp3,000'),
-                  Text('Umum: Rp3,500'),
-                ],
-              ), // row
-            ],
-          ), //column
-        ), // container
+        body: Card(
+          margin: const EdgeInsets.all(12),
+          child: ListTile(
+            leading: const Icon(Icons.inventory_2),
+            title: const Text(
+              'Buku Tulis',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            subtitle: const Text('Anggota Rp3.000 | Umum Rp3.500'),
+            trailing: Text(
+              'Stok  ' + stok.toString(),
+              style: TextStyle(color: stok == 0 ? Colors.red : Colors.black),
+            ),
+          ),
+        ),
       ), // scaffold
     ); // material app
   }
