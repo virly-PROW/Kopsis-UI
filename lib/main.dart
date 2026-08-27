db3e99c4eb0c4513b11ea26c9dc918a5eb5bfc8f
virly qoirul <virlyyannisa@gmail.com>
2026-08-27 12:20:56 +0700

    LKM-4 daftar barang

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,114 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   MyApp({super.key});
   int stok = 40;
+  String kategori = "ATK";
+
+  // FUNGSI UNTUK MEMILIH ICON OTOMATIS
+  IconData getIconByKategori(String kategori) {
+    switch (kategori) {
+      case 'ATK':
+        return Icons.edit;
+      case 'Makanan':
+        return Icons.fastfood;
+      case 'Minuman':
+        return Icons.local_drink;
+      default:
+        return Icons.inventory_2; // ikon default kalau kategori tidak dikenali
+    }
+  }
+
+  final List<Map<String, dynamic>> daftarBarang = [
+    {
+      'nama': 'Buku Tulis',
+      'kategori': 'ATK',
+      'harga_anggota': 3000,
+      'harga_umum': 3500,
+      'stok': 40,
+    },
+    {
+      'nama': 'Pensil',
+      'kategori': 'ATK',
+      'harga_anggota': 1000,
+      'harga_umum': 1500,
+      'stok': 50,
+    },
+    {
+      'nama': 'Penghapus',
+      'kategori': 'ATK',
+      'harga_anggota': 500,
+      'harga_umum': 700,
+      'stok': 30,
+    },
+    {
+      'nama': 'Roti',
+      'kategori': 'Makanan',
+      'harga_anggota': 2000,
+      'harga_umum': 2500,
+      'stok': 20,
+    },
+    {
+      'nama': 'Air Mineral',
+      'kategori': 'Minuman',
+      'harga_anggota': 1500,
+      'harga_umum': 2000,
+      'stok': 25,
+    },
+    {
+      'nama': 'Pensil Warna',
+      'kategori': 'ATK',
+      'harga_anggota': 5000,
+      'harga_umum': 6000,
+      'stok': 15,
+    },
+    {
+      'nama': 'Penggaris',
+      'kategori': 'ATK',
+      'harga_anggota': 2000,
+      'harga_umum': 2500,
+      'stok': 10,
+    },
+    {
+      'nama': 'Snack',
+      'kategori': 'Makanan',
+      'harga_anggota': 3000,
+      'harga_umum': 3500,
+      'stok': 30,
+    },
+    {
+      'nama': 'Teh Botol',
+      'kategori': 'Minuman',
+      'harga_anggota': 2500,
+      'harga_umum': 3000,
+      'stok': 20,
+    },
+    {
+      'nama': 'Pulpen',
+      'kategori': 'ATK',
+      'harga_anggota': 1500,
+      'harga_umum': 2000,
+      'stok': 35,
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: Card(
-          margin: const EdgeInsets.all(12),
-          child: ListTile(
-            leading: const Icon(Icons.inventory_2),
-            title: const Text(
-              'Buku Tulis',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            subtitle: const Text('Anggota Rp3.000 | Umum Rp3.500'),
-            trailing: Text(
-              'Stok  ' + stok.toString(),
-              style: TextStyle(color: stok == 0 ? Colors.red : Colors.black),
-            ),
-          ),
+        body: ListView.builder(
+          itemCount: daftarBarang.length,
+          itemBuilder: (context, index) {
+            final barang = daftarBarang[index];
+            return Card(
+              child: ListTile(
+                leading: const Icon(Icons.inventory_2),
+                title: Text(barang['nama']),
+                subtitle: Text(
+                  'Anggota Rp' + barang['harga_anggota'].toString(),
+                ),
+                trailing: Text('Stok: ' + barang['stok'].toString()),
+              ),
+            );
+          },
         ),
       ), // scaffold
     ); // material app
